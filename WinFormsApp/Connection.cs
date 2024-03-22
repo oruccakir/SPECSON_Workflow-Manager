73f39dffec0ba66d0cf717bd73bb47d1cee836a5
oruccakir <oruccakir2525@gmail.com>
2024-03-22 09:14:32 +0300

    connection class filled

--- WinFormsApp/Connection.cs
+++ WinFormsApp/Connection.cs
@@ -1,10 +1,10 @@
+using System.IO.Ports;  // Use this to communicate with the serial port
+using NModbus;          // Use this to communicate with the Modbus device  
+using EasyModbus;       // Use this to communicate with the Modbus device
 namespace WinFormsApp
 {
     // Connection class
     public class Connection : ConnectionInterface 
-    {
-     
-
-         
+    { 
     }
 }
